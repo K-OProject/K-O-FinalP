b9a013ff47c92c861b7dc1b3734453323cea544a
ahmedkortas <kortas.ahmed.dodo@gmail.com>
2020-08-20 08:35:58 -0700

    m last virsion

---
+++ js/reserve.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    var user = JSON.parse(localStorage.getItem('logIn'));
+    if (user.length>0){
+        $("body").css({"background": "url('file:///C:/Users/RBK/Desktop/K-O-FinalP/assets/bg.jpg')no-repeat","background-size":"100%"})
+}
+})
